3c988b7cd2447be2f8c451f183cefb86037e7219
703099162 <howard.mall@universalorlando.com>
2019-11-03 13:06:30 -0500

    The unity project.

---
+++ talking-pumpkin-unity/Assets/Scripts/AudioReactiveFlickering.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioReactiveFlickering : MonoBehaviour
+{
+    public string[] AudioInputs;
+    private AudioSource AudioSource;
+    public float Average;
+    public float[] Samples = new float[64];
+    public float FlickerSpeed = 0.1f;
+    private float timer = 0.0f;
+    public int MinimumFrequency;
+    public int MaximumFrequency;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+        SetAudioClipToMicrophone();
+        AudioSource.Play();
+    }
+
+    private void SetAudioClipToMicrophone()
+    {
+        AudioInputs = Microphone.devices;
+        Microphone.GetDeviceCaps(AudioInputs[0], out MinimumFrequency, out MaximumFrequency);
+        AudioSource.clip = Microphone.Start(AudioInputs[0], true, 1, MinimumFrequency);
+        AudioSource.loop = true;
+        while (!(Microphone.GetPosition(null) > 0) ){ }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        ComputeAverage();
+    }
+
+    void ComputeAverage()
+    {
+        float sum = 0.0f;
+        for( int i=0; i<64; i++)
+        {
+            sum += Samples[i];
+        }
+        Average = (sum/64.0f)*1000.0f;
+        if (Time.time >= timer)
+        {
+            Light flame = GetComponent<Light>();
+            flame.intensity = Average;
+            timer = Time.time + FlickerSpeed;
+        }
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        AudioSource.GetSpectrumData(Samples, 0, FFTWindow.Blackman);
+    }
+}
---
+++ talking-pumpkin-unity/Assets/Scripts/Flickering.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Flickering : MonoBehaviour
+{
+    public float FlickerSpeed = 0.1f;
+    private float timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= timer)
+        {
+            Light flame = GetComponent<Light>();
+            flame.intensity = Random.Range(0.5f, 2.5f);
+            timer = Time.time + FlickerSpeed;
+        }
+    }
+}
